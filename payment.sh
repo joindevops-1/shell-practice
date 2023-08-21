c7ec2dd56a9f1fa903d7be22177f15583e2063c5
sivakumarreddy <info@joindevops.com>
2023-08-21 12:58:05 +0530

    updated

--- payment.sh
+++ payment.sh
@@ -26,3 +26,18 @@ VALIDATE(){
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
+yum install python36 gcc python3-devel -y
+
+useradd roboshop
+
+mkdir /app 
+
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip
+
+cd /app 
+
+unzip /tmp/payment.zip
+
+pip3.6 install -r requirements.txt
+
